4e12d35b9c092dc6afb4d7bf1a6d5c76397fefee
sahasneil <119277552+sahasneil@users.noreply.github.com>
2025-01-03 14:46:40 +1100

    autosclae8

--- main.tf
+++ main.tf
@@ -38,17 +38,13 @@ module "autoscaling" {
   version = "8.0.1"
 
   name = "blog"
-  min_size = 1
-  max_size = 2
-
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-
-  target_group_arns   = [module.blog_alb.target_groups]
-
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-
-  image_id               = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
