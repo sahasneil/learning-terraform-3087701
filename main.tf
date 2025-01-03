148bc1a003be8a8034736fbae019ffba1a4bbb25
sahasneil <119277552+sahasneil@users.noreply.github.com>
2025-01-03 14:29:41 +1100

    autosclae2

--- main.tf
+++ main.tf
@@ -42,17 +42,12 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
   
   security_groups  = [module.blog_sg.security_group_id]
 
   image_id               = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-
-  load_balancers = [
-    {
-      target_group_arn = module.blog_alb.target_group_arns[0]
-    }
-  ]
 }
 
 module "blog_alb" {
